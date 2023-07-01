60848961d48b965b4f7008abc40f1bd313955f27
Normyx <mathieu.goulene@gmail.com>
2023-07-01 09:13:47 +0200

    Configure GitOps with NubesGen

---
+++ terraform/modules/app-service/main.tf
@@ -0,0 +1,70 @@
+terraform {
+  required_providers {
+    azurecaf = {
+      source  = "aztfmod/azurecaf"
+      version = "1.2.24"
+    }
+  }
+}
+
+resource "azurecaf_name" "app_service_plan" {
+  name          = var.application_name
+  resource_type = "azurerm_app_service_plan"
+  suffixes      = [var.environment]
+}
+
+# This creates the plan that the service use
+resource "azurerm_service_plan" "application" {
+  name                = azurecaf_name.app_service_plan.result
+  resource_group_name = var.resource_group
+  location            = var.location
+
+  sku_name = "F1"
+  os_type  = "Linux"
+
+  tags = {
+    "environment"      = var.environment
+    "application-name" = var.application_name
+  }
+}
+
+resource "azurecaf_name" "app_service" {
+  name          = var.application_name
+  resource_type = "azurerm_app_service"
+  suffixes      = [var.environment]
+}
+
+# This creates the service definition
+resource "azurerm_linux_web_app" "application" {
+  name                = azurecaf_name.app_service.result
+  resource_group_name = var.resource_group
+  location            = var.location
+  service_plan_id     = azurerm_service_plan.application.id
+  https_only          = true
+
+  tags = {
+    "environment"      = var.environment
+    "application-name" = var.application_name
+  }
+
+  site_config {
+    application_stack {
+      java_server         = "JAVA"
+      java_server_version = "11"
+      java_version        = "11"
+    }
+    always_on                 = false
+    ftps_state                = "FtpsOnly"
+  }
+
+  app_settings = {
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
+
+    # These are app specific environment variables
+    "SPRING_PROFILES_ACTIVE" = "prod,azure"
+
+    "SPRING_DATASOURCE_URL"      = "jdbc:mysql://${var.database_url}?useUnicode=true&characterEncoding=utf8&useSSL=true&useLegacyDatetimeCode=false&serverTimezone=UTC"
+    "SPRING_DATASOURCE_USERNAME" = var.database_username
+    "SPRING_DATASOURCE_PASSWORD" = var.database_password
+  }
+}
